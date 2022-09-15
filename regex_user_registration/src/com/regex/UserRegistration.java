8ccd70a28f3bf2dd139f44698f10cec08f3bfa24
Nikhiljain283 <nikhiljainsgrl283@gmail.com>
2022-09-15 11:46:42 +0530

    last name start with capital letter.

--- regex_user_registration/src/com/regex/UserRegistration.java
+++ regex_user_registration/src/com/regex/UserRegistration.java
@@ -1,9 +1,17 @@
 package com.regex;
 
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
+
+		boolean firstName = Pattern.matches("^[A-Z]{1}[a-zA-Z]{1}[a-zA-Z]+$", "Aman");
+		System.out.println(firstName);
+
+		boolean lastName = Pattern.matches("^[A-Z]{1}[a-zA-Z]{1}[a-zA-Z]+$", "Singh");
+		System.out.println(lastName);
 
 	}
 
